913f0724b58c136725a000eff74af7c30dbd52b4
Tim Vincent <vincent3@purdue.edu>
2017-01-21 13:04:30 -0500

    Added results to dict

--- scraper.py
+++ scraper.py
@@ -4,17 +4,23 @@ from bs4 import BeautifulSoup
 
 split = '%2C'
 base_site = 'http://www.epicurious.com'
-def search_bot(ingredients):
+def recipe_search(ingredients):
+	recipe = {}
 	payload = '/search/?content=recipe&include='
 	for ingredient in ingredients:
 		payload = payload + ingredient + split
 	r = requests.get(base_site + payload) #Todo, remove last 3 chars
 	soup = BeautifulSoup(r.content, "html.parser")
 	results = soup.findAll('article', attrs={'class': 'recipe-content-card'})
+
 	for result in results: 
-		recipe = result.findAll('a')[0]
-		print recipe.text #title
-		print base_site + recipe.get('href') #url
+		raw_recipe = result.findAll('a')[0]
+		title = raw_recipe.text
+		url = base_site + raw_recipe.get('href')
+		recipe[title] = url
+
+	return recipe
 
 if __name__ == "__main__":
-	search_bot(['bacon', 'eggs'])
+	print recipe_search(['bacon', 'eggs'])
+
