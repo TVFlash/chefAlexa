97e562692635ffed99769b0c0b5a579e989190f5
Tim Vincent <vincent3@purdue.edu>
2017-02-12 15:02:52 -0500

    Removed excessive whitespace

--- skill.py
+++ skill.py
@@ -1,11 +1,7 @@
 import logging
-
 from random import randint
-
 from flask import Flask, render_template
-
 from flask_ask import Ask, statement, question, session
-
 from scraper import recipe_search, recipe_parse
 
 app = Flask(__name__)
@@ -15,13 +11,9 @@ ask = Ask(app, "/")
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
 follow_up = " Can I do anything else for you today?"
-
 step_prompt = " Say next step to continue or last step to hear this step again."
-
 instructions = []
-
 fridge = []
-
 dish = ""
 
 index = 0
@@ -125,7 +117,6 @@ def remove_fridge():
 def finish_cooking():
 
     return statement("Goodbye")
-#TODO, turn add / remove into questions, create noIntent if the user no longer wishes to continue
 
 if __name__ == '__main__':
 
